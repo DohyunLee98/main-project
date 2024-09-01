5fd666e891c40dc6d26bbf2ff89326776600e8df
LEE DO HYUN <leedh9806@naver.com>
2024-09-01 19:03:23 +0900

    09-01 19:03 헤더 추가

---
+++ src/components/common/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../../styles/Header.css";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // 로그아웃 로직을 여기에 추가
+    navigate("/");
+  };
+
+  return (
+    <header className="header-container">
+      <div className="header-content">
+        <div className="logo">Logo</div>
+        <div className="spacer"></div>
+        <button className="logout-button" onClick={handleLogout}>
+          로그아웃
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -4,6 +4,7 @@ import DirectoryViewer from "../components/project/document/Documents";
 import Invite from "../components/project/member/Invite";
 import Schedule from "../components/project/schedule/Schedule";
 import Title from "../components/project/Title";
+import Header from "../components/common/Header";
 import "../styles/ProjectPage.css";
 
 const ProjectPage = () => {
@@ -11,22 +12,25 @@ const ProjectPage = () => {
   const project = location.state || {};
 
   return (
-    <div className="project-page-container">
-      {/* 프로젝트 제목과 공지사항 */}
-      <div className="project-section">
-        <Title project={project[0]} />
-      </div>
-      {/* 일정 섹션: 캘린더, 칸반, 간트차트 */}
-      <div className="project-section">
-        <Schedule />
-      </div>
-      {/* 문서 관리 섹션 */}
-      <div className="project-section">
-        <DirectoryViewer />
-      </div>
-      {/* 멤버 관리 섹션 */}
-      <div className="project-section">
-        <Invite />
+    <div>
+      <Header />
+      <div className="project-page-container">
+        {/* 프로젝트 제목과 공지사항 */}
+        <div className="project-section">
+          <Title project={project[0]} />
+        </div>
+        {/* 일정 섹션: 캘린더, 칸반, 간트차트 */}
+        <div className="project-section">
+          <Schedule />
+        </div>
+        {/* 문서 관리 섹션 */}
+        <div className="project-section">
+          <DirectoryViewer />
+        </div>
+        {/* 멤버 관리 섹션 */}
+        <div className="project-section">
+          <Invite />
+        </div>
       </div>
     </div>
   );
